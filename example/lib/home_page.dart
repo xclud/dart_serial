e4364ccd93a844994db8204aa6e5e1792dc2756e
Mahdi K. Fard <mkfard@outlook.com>
2023-01-04 11:57:36 +0330

    Improve the demo app UI.

---
+++ example/lib/home_page.dart
@@ -0,0 +1,154 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
+import 'dart:async';
+import 'dart:html';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import 'package:serial/serial.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  SerialPort? _port;
+  final _received = <Uint8List>[];
+
+  final _controller1 = TextEditingController();
+
+  Future<void> _openPort() async {
+    await _port?.close();
+
+    final port = await window.navigator.serial.requestPort();
+    await port.open(baudRate: 9600);
+
+    _port = port;
+
+    _startReceiving(port);
+
+    setState(() {});
+  }
+
+  Future<void> _writeToPort(Uint8List data) async {
+    if (data.isEmpty) {
+      return;
+    }
+
+    final port = _port;
+
+    if (port == null) {
+      return;
+    }
+
+    final writer = port.writable.writer;
+
+    await writer.ready;
+    await writer.write(data);
+
+    await writer.ready;
+    await writer.close();
+  }
+
+  Future<void> _startReceiving(SerialPort port) async {
+    final reader = port.readable.reader;
+
+    while (true) {
+      final result = await reader.read();
+      _received.add(result.value);
+
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Serial Port'),
+        actions: [
+          IconButton(
+            onPressed: _openPort,
+            icon: Icon(Icons.device_hub),
+            tooltip: 'Open Serial Port',
+          ),
+          IconButton(
+            onPressed: _port == null
+                ? null
+                : () async {
+                    await _port?.close();
+                    _port = null;
+
+                    setState(() {});
+                  },
+            icon: Icon(Icons.close),
+            tooltip: 'Close Serial Port',
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white54),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: _received.isNotEmpty
+                    ? ListView(
+                        padding: const EdgeInsets.all(4),
+                        children: _received.map((e) {
+                          final text = String.fromCharCodes(e);
+                          return Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(text),
+                          );
+                        }).toList(),
+                      )
+                    : Center(
+                        child: Text(
+                        'No data received yet.',
+                        textAlign: TextAlign.center,
+                      )),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: TextFormField(
+                    controller: _controller1,
+                  ),
+                ),
+                Gap(8),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ElevatedButton(
+                    child: const Text('Send'),
+                    onPressed: () {
+                      _writeToPort(
+                          Uint8List.fromList(_controller1.text.codeUnits));
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,7 @@
 // ignore_for_file: avoid_web_libraries_in_flutter
 
-import 'dart:async';
-import 'dart:html';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-
-import 'package:serial/serial.dart';
+import 'package:example/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,55 +15,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  SerialPort? _port;
-  final _received = <String>[];
-
-  Future<void> _openPort() async {
-    await _port?.close();
-
-    final port = await window.navigator.serial.requestPort();
-    await port.open(baudRate: 9600);
-
-    _port = port;
-
-    setState(() {});
-  }
-
-  Future<void> _writeToPort() async {
-    final port = _port;
-
-    if (port == null) {
-      return;
-    }
-
-    final writer = port.writable.writer;
-
-    await writer.ready;
-    await writer.write(Uint8List.fromList('Hello World.'.codeUnits));
-
-    await writer.ready;
-    await writer.close();
-  }
-
-  Future<void> _readFromPort() async {
-    final port = _port;
-
-    if (port == null) {
-      return;
-    }
-
-    final reader = port.readable.reader;
-
-    while (true) {
-      final result = await reader.read();
-      final text = String.fromCharCodes(result.value);
-
-      _received.add(text);
-
-      setState(() {});
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final theme = ThemeData(
@@ -77,59 +24,13 @@ class _MyAppState extends State<MyApp> {
       inputDecorationTheme: InputDecorationTheme(
         border: OutlineInputBorder(),
         alignLabelWithHint: true,
+        isDense: true,
       ),
     );
 
     return MaterialApp(
       theme: theme,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Serial Port'),
-          actions: [
-            IconButton(
-              onPressed: _openPort,
-              icon: Icon(Icons.device_hub),
-              tooltip: 'Open Serial Port',
-            ),
-            IconButton(
-              onPressed: _port == null
-                  ? null
-                  : () async {
-                      await _port?.close();
-                      _port = null;
-
-                      setState(() {});
-                    },
-              icon: Icon(Icons.close),
-              tooltip: 'Close Serial Port',
-            ),
-          ],
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.all(8),
-                children: _received.map((e) => Text(e)).toList(),
-              ),
-            ),
-            ElevatedButton(
-              child: const Text('Send'),
-              onPressed: () {
-                _writeToPort();
-              },
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              child: const Text('Receive'),
-              onPressed: () {
-                _readFromPort();
-              },
-            ),
-          ],
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
