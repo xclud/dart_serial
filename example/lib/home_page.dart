337f52281bb69556a652354d821ceafa9419f967
Mahdi <mkfard@outlook.com>
2023-07-08 23:08:20 +0330

    Updated the Dart SDK and dependencies.

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -67,7 +67,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    //final theme = Theme.of(context);
 
     return Scaffold(
       appBar: AppBar(
